64e299205e9fc7855204cb95a5324dda369c0b07
Tatiana Inama <tatii.oxo@gmail.com>
2020-06-29 16:37:08 +0200

    Load distributors data on DB

--- server/db/index.js
+++ server/db/index.js
@@ -1,19 +1,26 @@
-import DATA from './products.json';
+import PRODUCTS from './products.json';
+import DISTRIBUTORS from './distributors.json';
 
-const initDB = async (db) => {
-  db.createCollection('products', {
+const createCollection = (db) => async (collectionName, data) => {
+  db.createCollection(collectionName, {
     collation: { locale: 'es' }
   });
-
-  const col = db.collection('products');
-  col.find({}).toArray().then(result => {
-    if(!result.length) {
-      col.insertMany(DATA).then(insertResult => {
-        console.log('products collection created correctly')
-      })
+  const col = db.collection(collectionName);
+  try {
+    const results = await col.find({}).toArray();
+    if (!results.length) {
+      console.log(`creating ${collectionName}...`);
+      await col.insertMany(data)
+      console.log(`${collectionName} created correctly`);
     }
-  });
-  
+  } catch(error) {
+    throw new Error(`Error creating collection ${collectionName}: ${error}`);
+  }
+}
+
+const initDB = async (db) => {
+  await createCollection(db)('products', PRODUCTS);
+  await createCollection(db)('distributors', DISTRIBUTORS);
 }
 
 export default initDB;
